9507c0fb4ae5d838c9172a0ebf14f61f36c341f3
ubuntu <zhaosm15@mails.tsinghua.edu.cn>
2017-11-30 10:54:26 +0800

    add cors

--- file_manager/views.py
+++ file_manager/views.py
@@ -3,6 +3,8 @@ from file_manager.models import myFile
 from file_manager.forms import FileForm
 from file_service import settings
 
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -10,9 +12,8 @@ import json
 
 __author__ = "Bifei Yang"
 
-
+@method_decorator(csrf_exempt, name='dispatch')
 class FileView(BaseView):
-
     def post(self):
         self.check_input('fileType', 'validTime')
         new_file = myFile(
@@ -23,13 +24,13 @@ class FileView(BaseView):
         )
         new_file.save()
 
-        new_file.fileUrl = settings.CONFIGS['SITE_DOMAIN'] + ":8000" + new_file.file.url
-
-        return HttpResponse(json.dumps({
+        new_file.fileUrl = settings.CONFIGS['SITE_DOMAIN'] + new_file.file.url
+        response =  HttpResponse(json.dumps({
             'code': 0,
             'msg': "",
             'data': new_file.fileUrl
         }), content_type="application/json")
+        return response
 
     def get(self):
         files = myFile.objects.all()
