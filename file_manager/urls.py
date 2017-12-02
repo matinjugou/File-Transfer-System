30dc69e5402c8805a7d5ef68cd2016d25f5a4eb8
effie-0 <thss15yangbf@gmail.com>
2017-12-02 21:28:59 +0800

    Add version2. Can support returning file types.

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from file_manager.views import FileView
+from file_manager.views import FileView, FileView2
 
 urlpatterns = [
-    url(r'file/$', FileView.as_view(), name="file")
+    url(r'v1/file/$', FileView.as_view(), name="file"),
+    url(r'v2/file/$', FileView2.as_view(), name="file2")
 ]
--- file_manager/views.py
+++ file_manager/views.py
@@ -12,6 +12,7 @@ import json
 
 __author__ = "Bifei Yang"
 
+
 @method_decorator(csrf_exempt, name='dispatch')
 class FileView(BaseView):
     def post(self):
@@ -25,7 +26,7 @@ class FileView(BaseView):
         new_file.save()
 
         new_file.fileUrl = settings.CONFIGS['SITE_DOMAIN'] + new_file.file.url
-        response =  HttpResponse(json.dumps({
+        response = HttpResponse(json.dumps({
             'code': 0,
             'msg': "",
             'data': new_file.fileUrl
@@ -36,3 +37,34 @@ class FileView(BaseView):
         files = myFile.objects.all()
         form = FileForm()
         return render(self.request, 'fileList.html', {'files': files, 'form': form})
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class FileView2(BaseView):
+    def post(self):
+        self.check_input('validTime')
+        new_file = myFile(
+            fileType='',
+            validTime=self.input['validTime'],
+            fileUrl='',
+            file=self.request.FILES['file']
+        )
+        new_file.save()
+        new_file.file.close()
+
+        new_file.fileType = self.request.FILES['file'].content_type
+        new_file.fileUrl = settings.CONFIGS['SITE_DOMAIN'] + new_file.file.url
+        new_file.save()
+
+        response = HttpResponse(json.dumps({
+            'code': 0,
+            'msg': "",
+            'data': new_file.fileUrl,
+            'type': new_file.fileType
+        }), content_type="application/json")
+        return response
+
+    def get(self):
+        files = myFile.objects.all()
+        form = FileForm()
+        return render(self.request, 'fileList2.html', {'files': files, 'form': form})
